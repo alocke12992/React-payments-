fddaf32937d752b212355bc58ca78034f6e3049d
Andrew Locke <alocke12992@gmail.com>
2018-04-17 10:49:05 -0600

    Adds basic components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {
+  Header,
+  Segment,
+  Input,
+  Label,
+  Divider,
+  Image,
+} from 'semantic-ui-react';
+import BraintreeDrop from './BraintreeDrop';
 
 class Home extends Component {
+  state = {amount: 150.0}
+
   render() {
+    const {amount} = this.state
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <Segment basic textAlign="center">
+        <Header
+          as="h1"
+          textAlign="center"
+        >
+          React Payments
+        </Header>
+        <Image
+          centered
+          size="small"
+          src="https://farm1.staticflickr.com/909/39713108170_7198ebbb81_o.jpg"
+          alt="food basket"
+        />
+        <Label color="green">
+          Payment Amount
+        </Label>
+        <Input
+          value={amount}
+          disabled
+          style={{fontSize: '18px'}}
+        />
+        <Divider />
+        <BraintreeDrop amount={amount} />
+      </Segment>
     );
   }
 }
 
 export default Home;
+
+//https://farm1.staticflickr.com/909/39713108170_7198ebbb81_o.jpg
